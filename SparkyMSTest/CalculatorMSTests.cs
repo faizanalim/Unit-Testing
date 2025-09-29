4b2bef499aedee49601058f53e472eac68a57e05
faizanalim <faizan.23@hotmail.com>
2025-09-30 03:31:31 +0500

    Add project files.

---
+++ SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,28 @@
+﻿using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new Calculator();
+            int a = 10;
+            int b = 20;
+
+            //Act
+            int result = calc.AddNumbers(a, b);
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+    }
+}
---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new();
+            //Act
+            int result = calc.AddNumbers(10, 40);
+            //Assert
+            Assert.That(result, Is.EqualTo(30));
+        }
+    }
+}
