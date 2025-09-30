bdd98bc4a2c2113e345b55af05f5658f67f74665
faizanalim <faizan.23@hotmail.com>
2025-09-30 05:21:48 +0500

    Unit Test with Fake Logger

---
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class BankAccountNUnitTests
+    {
+        private BankAccount bankAccount;
+        [SetUp]
+        public void Steup()
+        {
+            bankAccount = new(new LogFakker());
+        }
+        [Test]
+        public void BankDeposit_Add100_ReturnTrue()
+        {
+            var result = bankAccount.Deposit(100);
+            Assert.That(result,Is.True);
+            Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+        }
+
+    }
+}
